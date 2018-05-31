96b2f9f8ea8fd15e6d8ab3a4a7be769b0b432b0c
Marco Lackovic <marco.lackovic@gmail.com>
2018-05-31 14:59:09 +0300

    Remove logging

--- src/app/shared/validator/areDifferent.ts
+++ src/app/shared/validator/areDifferent.ts
@@ -4,8 +4,6 @@ export function areDifferent(controlNames: string[]) {
   return (control: AbstractControl): null | ValidationErrors => {
     const value1 = control.get(controlNames[0]).value;
     const value2 = control.get(controlNames[1]).value;
-    console.log('control.get(controlNames[0]).value = ' + value1);
-    console.log('control.get(controlNames[1]).value = ' + value2);
     if (value1 === value2) {
       return {
         areDifferent: {
